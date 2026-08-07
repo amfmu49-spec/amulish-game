4a1addba6036d07cb1c356a131b616fdc394d1e3
amfmu49-spec <amfmu49-spec@users.noreply.github.com>
2026-08-08 08:57:30 +0900

    Updates

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,6 @@
 (function () {
   (async function () {
-    const VER = 'v2.2.3';
+    const VER = 'v2.3.0';
     const AMULISH_URL = 'https://amfmu49-spec.github.io/amulish-game/';
 
     // --- Show toast/overlay notification on SUNO page ---
